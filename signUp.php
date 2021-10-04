ea10e1d0c15761ee070543b912efa87723497f66
kweziEpsi <kwezi@digilims.com>
2021-10-04 15:17:44 +0200

    Added signUp validation code

---
+++ signUp.php
@@ -0,0 +1,22 @@
+<?php
+
+$first_name = "";
+$last_name = "";
+$username =""; 
+$email ="";
+$gender="";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $first_name = test_input($_POST["first_name"]);
+  $last_name = test_input($_POST["last_name"]);
+  $email = test_input($_POST["email"]);
+  $gender = test_input($_POST["gender"]);
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
